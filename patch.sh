c4f29d5709d3ace08ef4b036ee5ff1d788246b47
Alfie Richardson <01richardsonalfie@protonmail.com>
2023-04-25 21:05:11 +0100

    fixed copy command in scripts

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This disables operating-system level mitigations and page table isolation
-copy ./grub-configs/patch-grub /etc/default/grub
+cp ./grub-configs/patch-grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Please reboot your system"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This disables operating-system level mitigations and page table isolation
-copy ./grub-configs/vuln-grub /etc/default/grub
+cp ./grub-configs/vuln-grub /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Please reboot your system"
